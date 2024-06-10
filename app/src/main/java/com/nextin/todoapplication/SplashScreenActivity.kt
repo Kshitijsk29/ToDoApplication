28f74876bcd2cdb1856d6bed35c0f1db3d399704
kshitij <albatros.29kr@gmail.com>
2024-06-10 20:38:56 +0530

    Initial commit

---
+++ app/src/main/java/com/nextin/todoapplication/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.nextin.todoapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+
+       Handler(Looper.getMainLooper()).postDelayed({
+           val intent = Intent(this@SplashScreenActivity , MainActivity::class.java)
+           startActivity(intent)
+           finish()
+       }, 3000)
+
+    }
+}
